e266dcbdb946da5ed6334c994e0be31073e2dd32
khoa huynh <khoahuynh1901@gmail.com>
2024-06-04 11:24:34 -0400

    Resume

---
+++ js/autotypinganimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Software Developer</span>"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 1000);
